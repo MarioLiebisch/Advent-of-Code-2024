f9248e24ab30e0472912ae346ee96ac1d3f9be92
Mario Liebisch <mario.liebisch@gmail.com>
2024-12-01 09:36:11 +0100

    Allow return values

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import {argv} from 'node:process';
+import process from 'node:process';
 
 
 const main = async () => {
+    const argv = process.argv;
     if (argv.length < 3) {
         console.error('Missing day number as command line argument!');
         return;
     }
     const script = await import(`./day-${argv[2].padStart(2, '0')}.js`);
-    script.main(argv.slice(3));
+    process.exitCode = script.main(argv.slice(3));
 };
 
 main();
