c83b2deac8a0bc5053fe2be0329b6dceeaeb26c8
Mario Liebisch <mario.liebisch@gmail.com>
2024-12-01 12:58:58 +0100

    Updated loader

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,33 @@
 import process from "node:process";
+import fs from "node:fs/promises";
 
 const main = async () => {
   const argv = process.argv;
-  console.log(argv);
+
   if (argv.length < 3) {
     console.error("Missing day as command line argument!");
     return;
   }
 
   let file = argv[2];
-  if (file.startsWith("day-")) {
-    file = file.slice(4);
-  }
-  if (file.endsWith(".ts")) {
-    file = file.slice(0, -3);
+  if (file.match(/^\d+$/)) {
+    file = file.padStart(2, "0");
+    file = `./day-${file}.js`;
+  } else {
+    if (file.endsWith(".ts")) {
+      file = file.slice(0, -3);
+    }
+    file = `./${file}.js`;
   }
 
-  const script = await import(`./day-${file}.js`);
-  process.exitCode = script.main(argv.slice(3));
+  try {
+    await fs.access(file);
+    const script = await import(file);
+    process.exitCode = script.main(argv.slice(3));
+  } catch {
+    console.error(`File ${file} does not exist!`);
+    return;
+  }
 };
 
 main();
